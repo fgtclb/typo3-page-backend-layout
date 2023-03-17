fd8eea8eb4e288b1a6519328a06362fe84a1d942
Markus Hofmann <m.hofmann@web-vision.de>
2023-03-17 13:44:41 +0100

    [TASK] Add dynamic page layout rendering

---
+++ ext_emconf.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => '(FGTCLB) Page Backend Layout',
+    'description' => 'Helper for Backend layout overrides based on page doktype',
+    'category' => 'be',
+    'state' => 'beta',
+    'version' => '1.0.0',
+];
