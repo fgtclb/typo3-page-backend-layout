caf510fc8807e7257fc47eb7dcabd8a96c752250
mhofmann <m.hofmann@web-vision.de>
2023-09-19 18:25:29 +0200

    [TASK] Prepare release

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 $EM_CONF[$_EXTKEY] = [
     'title' => '(FGTCLB) Page Backend Layout',
     'description' => 'Helper for Backend layout overrides based on page doktype',
     'category' => 'be',
     'state' => 'stable',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
