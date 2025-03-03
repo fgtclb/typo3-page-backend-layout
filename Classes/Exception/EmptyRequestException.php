337b810e1d3eb34c2ba1895c2bf35366adcc139e
Markus Hofmann <m.hofmann@web-vision.de>
2025-03-03 10:19:13 +0100

    [TASK] Run code style fixer

--- Classes/Exception/EmptyRequestException.php
+++ Classes/Exception/EmptyRequestException.php
@@ -4,7 +4,4 @@ declare(strict_types=1);
 
 namespace FGTCLB\PageBackendLayout\Exception;
 
-final class EmptyRequestException extends \Exception
-{
-
-}
+final class EmptyRequestException extends \Exception {}
