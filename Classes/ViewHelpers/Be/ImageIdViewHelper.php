cb437a56f7dc48e16b3283e03fe3b031602f84af
Markus Hofmann <m.hofmann@web-vision.de>
2025-03-03 10:17:22 +0100

    [TASK] Adjust error handling
    Check for empty request and change general runtimeException
    with specific ones.

--- Classes/ViewHelpers/Be/ImageIdViewHelper.php
+++ Classes/ViewHelpers/Be/ImageIdViewHelper.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace FGTCLB\PageBackendLayout\ViewHelpers\Be;
 
 use Doctrine\DBAL\Exception;
+use FGTCLB\PageBackendLayout\Exception\EmptyRequestException;
+use FGTCLB\PageBackendLayout\Exception\WrongContextException;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Http\ApplicationType;
 use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
 use TYPO3\CMS\Core\Resource\FileReference;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContext;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
 class ImageIdViewHelper extends AbstractViewHelper
@@ -21,13 +24,15 @@ class ImageIdViewHelper extends AbstractViewHelper
         $this->registerArgument('table', 'string', 'The table', true);
         $this->registerArgument('field', 'string', 'Field matching', true);
         $this->registerArgument('uid', 'int', 'Integer of current record', true);
-        $this->registerArgument('returnFirst', 'bool', 'Whether to return only the frist value', false, true);
+        $this->registerArgument('returnFirst', 'bool', 'Whether to return only the first value', false, true);
         $this->registerArgument('as', 'string', 'Variable name to return', false, 'image');
     }
 
     /**
      * @throws FileDoesNotExistException
      * @throws Exception
+     * @throws EmptyRequestException
+     * @throws WrongContextException
      */
     public function render(): string
     {
@@ -41,14 +46,21 @@ class ImageIdViewHelper extends AbstractViewHelper
          * } $arguments
          */
         $arguments = $this->arguments;
+        /** @var RenderingContext $renderingContext */
         $renderingContext = $this->renderingContext;
         $renderChildrenClosure = $this->renderChildrenClosure;
 
         $request = $renderingContext->getRequest();
+        if ($request === null) {
+            throw new EmptyRequestException(
+                'ViewHelper can only be called with a proper request',
+                1740993136
+            );
+        }
         $isBackendRequest = $request->getAttribute('applicationType')
             && ApplicationType::fromRequest($request)->isBackend();
         if (!$isBackendRequest) {
-            throw new \RuntimeException(
+            throw new WrongContextException(
                 'ViewHelper can only be called in backend context',
                 1683720517279
             );
