f6468a6d738f0fbfca2f46c466a62a4474cf1d24
Markus Hofmann <m.hofmann@web-vision.de>
2025-03-03 10:19:13 +0100

    [TASK] Run code style fixer

--- Classes/Exception/WrongContextException.php
+++ Classes/Exception/WrongContextException.php
@@ -4,7 +4,4 @@ declare(strict_types=1);
 
 namespace FGTCLB\PageBackendLayout\Exception;
 
-final class WrongContextException extends \Exception
-{
-
-}
+final class WrongContextException extends \Exception {}
