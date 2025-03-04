18fec7794a4aebdb3396640fb2a2e83f3c0b99d7
Stefan Bürk <stefan@buerk.tech>
2025-03-04 18:53:42 +0100

    [RELEASE] 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'FGTCLB',
     'author_email' => 'hello@fgtclb.com',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.3',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.5.99',
+            'typo3' => '11.5.0-12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
